4348b5a2c717cdc20fb62fdd341bec814046fa58
tunglam16402 <lamnguyen16042002@gmail.com>
2025-04-13 22:24:16 +0700

    upstash config

--- lib/config.ts
+++ lib/config.ts
@@ -11,6 +11,8 @@ const config = {
     upstash: {
       redisUrl: process.env.UPSTASH_REDIS_REST_URL!,
       redisToken: process.env.UPSTASH_REDIS_REST_TOKEN!,
+      qstashUrl: process.env.QSTASH_URL!,
+      qstashToken: process.env.QSTASH_TOKEN!,
     },
   },
 };
