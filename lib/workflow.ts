5a66d86820e4b7ae687746e560bbab2c74b121fc
tunglam16402 <lamnguyen16042002@gmail.com>
2025-04-14 15:49:01 +0700

    workflow adjust

--- lib/workflow.ts
+++ lib/workflow.ts
@@ -1,5 +1,5 @@
 import { Client as WorkflowClient } from "@upstash/workflow";
-// import { Client as QStashClient, resend } from "@upstash/qstash";git
+import { Client as QStashClient, resend } from "@upstash/qstash";
 import config from "@/lib/config";
 
 export const workflowClient = new WorkflowClient({
@@ -7,29 +7,29 @@ export const workflowClient = new WorkflowClient({
   token: config.env.upstash.qstashToken,
 });
 
-// const qstashClient = new QStashClient({
-//   token: config.env.upstash.qstashToken,
-// });
+const qstashClient = new QStashClient({
+  token: config.env.upstash.qstashToken,
+});
 
-// export const sendEmail = async ({
-//   email,
-//   subject,
-//   message,
-// }: {
-//   email: string;
-//   subject: string;
-//   message: string;
-// }) => {
-//   await qstashClient.publishJSON({
-//     api: {
-//       name: "email",
-//       provider: resend({ token: config.env.resendToken }),
-//     },
-//     body: {
-//       from: "JS Mastery <contact@adrianjsmastery.com>",
-//       to: [email],
-//       subject,
-//       html: message,
-//     },
-//   });
-// };
+export const sendEmail = async ({
+  email,
+  subject,
+  message,
+}: {
+  email: string;
+  subject: string;
+  message: string;
+}) => {
+  await qstashClient.publishJSON({
+    api: {
+      name: "email",
+      provider: resend({ token: config.env.upstash.qstashToken }),
+    },
+    body: {
+      from: "JS Mastery <contact@adrianjsmastery.com>",
+      to: [email],
+      subject,
+      html: message,
+    },
+  });
+};
