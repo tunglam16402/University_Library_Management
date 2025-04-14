6296fc5319bd6ca3c61bf51feb3692b79ef9ceb3
tunglam16402 <lamnguyen16042002@gmail.com>
2025-04-14 22:19:47 +0700

    a

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,12 +1,35 @@
 import { auth } from "@/auth";
 import Header from "@/components/Header";
+import { db } from "@/database/drizzle";
+import { users } from "@/database/schema";
+import { eq } from "drizzle-orm";
 import { redirect } from "next/navigation";
+import { after } from "next/server";
 import React, { ReactNode } from "react";
 
 const layout = async ({ children }: { children: ReactNode }) => {
   const session = await auth();
 
   if (!session) redirect("/login");
+
+  after(async () => {
+    if (!session?.user?.id) return;
+
+    const user = await db
+      .select()
+      .from(users)
+      .where(eq(users.id, session?.user?.id))
+      .limit(1);
+
+    if (user[0].lastActivityDate === new Date().toISOString().slice(0, 10))
+      return;
+
+    await db
+      .update(users)
+      .set({ lastActivityDate: new Date().toISOString().slice(0, 10) })
+      .where(eq(users.id, session?.user?.id));
+  });
+
   return (
     <main className="root-container">
       <div className="mx-auto max-w-7xl">
--- lib/workflow.ts
+++ lib/workflow.ts
@@ -2,11 +2,13 @@ import { Client as WorkflowClient } from "@upstash/workflow";
 import { Client as QStashClient, resend } from "@upstash/qstash";
 import config from "@/lib/config";
 
+// Khởi tạo WorkflowClient để gọi các workflow
 export const workflowClient = new WorkflowClient({
   baseUrl: config.env.upstash.qstashUrl,
   token: config.env.upstash.qstashToken,
 });
 
+// Khởi tạo QStashClient để gửi email qua Resend
 const qstashClient = new QStashClient({
   token: config.env.upstash.qstashToken,
 });
@@ -20,16 +22,17 @@ export const sendEmail = async ({
   subject: string;
   message: string;
 }) => {
+  // Chắc chắn rằng bạn dùng email của Resend trong môi trường Dev
   await qstashClient.publishJSON({
     api: {
       name: "email",
-      provider: resend({ token: config.env.upstash.qstashToken }),
+      provider: resend({ token: config.env.resendToken }), // Sử dụng API Key của Resend
     },
     body: {
-      from: "JS Mastery <contact@adrianjsmastery.com>",
-      to: [email],
+      from: "Project Dev <onboarding@resend.dev>", // Email mặc định của Resend
+      to: [email], // Địa chỉ email nhận
       subject,
-      html: message,
+      html: message, // Nội dung HTML của email
     },
   });
 };
