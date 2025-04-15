b20474b87229e1afc41ba272d63e8aeac4e03b16
tunglam16402 <lamnguyen16042002@gmail.com>
2025-04-15 16:14:02 +0700

    fileupload1

--- components/auth/FileUpload.tsx
+++ components/auth/FileUpload.tsx
@@ -16,7 +16,8 @@ const {
 
 const authenticator = async () => {
   try {
-    const response = await fetch(`${config.env.apiEndpoint}/api/imagekit`);
+    // const response = await fetch(`${config.env.apiEndpoint}/api/imagekit`);
+    const response = await fetch("/api/imagekit");
 
     if (!response.ok) {
       const errorText = await response.text();
