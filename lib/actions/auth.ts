2c17b366903d6f81409fae5d6e7123dbf3136057
tunglam16402 <lamnguyen16042002@gmail.com>
2025-04-15 15:28:52 +0700

    qstash workflows

--- lib/actions/auth.ts
+++ lib/actions/auth.ts
@@ -8,8 +8,8 @@ import { signIn } from "@/auth";
 import { headers } from "next/headers";
 import ratelimit from "../rateLimit";
 import { redirect } from "next/navigation";
-// import { workflowClient } from "@/lib/workflow";
-// import config from "@/lib/config";
+import { workflowClient } from "@/lib/workflow";
+import config from "@/lib/config";
 
 export const signInWithCredentials = async (
   params: Pick<AuthCredentials, "email" | "password">
@@ -68,13 +68,13 @@ export const signUp = async (params: AuthCredentials) => {
       universityCard,
     });
 
-    // await workflowClient.trigger({
-    //   url: `${config.env.prodApiEndpoint}/api/workflows/onboarding`,
-    //   body: {
-    //     email,
-    //     fullName,
-    //   },
-    // });
+    await workflowClient.trigger({
+      url: `${config.env.prodApiEndpoint}/api/workflows/onboarding`,
+      body: {
+        email,
+        fullName,
+      },
+    });
 
     await signInWithCredentials({ email, password });
 
