681063c4aa7705adb95070d1e0a716c646bbba74
tunglam16402 <lamnguyen16042002@gmail.com>
2025-04-16 16:40:57 +0700

    admin layout

--- constants/index.ts
+++ constants/index.ts
@@ -151,3 +151,45 @@ export const FIELD_TYPES = {
   password: "password",
   confirmPassword: "password",
 };
+
+export const navigationLinks = [
+  {
+    href: "/library",
+    label: "Library",
+  },
+
+  {
+    img: "/icons/user.svg",
+    selectedImg: "/icons/user-fill.svg",
+    href: "/my-profile",
+    label: "My Profile",
+  },
+];
+
+export const adminSideBarLinks = [
+  {
+    img: "/icons/admin/home.svg",
+    route: "/admin",
+    text: "Home",
+  },
+  {
+    img: "/icons/admin/users.svg",
+    route: "/admin/users",
+    text: "All Users",
+  },
+  {
+    img: "/icons/admin/book.svg",
+    route: "/admin/books",
+    text: "All Books",
+  },
+  {
+    img: "/icons/admin/bookmark.svg",
+    route: "/admin/book-requests",
+    text: "Borrow Requests",
+  },
+  {
+    img: "/icons/admin/user.svg",
+    route: "/admin/account-requests",
+    text: "Account Requests",
+  },
+];
